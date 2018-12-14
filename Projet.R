98beca4d5c96809c14af278322edacf1834301d9
CHENTing <krystal_0902@icloud.com>
2018-12-14 23:14:00 +0100

    somes problems in the code

--- Projet.R
+++ Projet.R
@@ -1,11 +1,23 @@
 # setwd("/home/t_chen/travaux/5annee/BigData/")
 
 df <- read.csv("athlete_events.csv", sep = ",")
+# select the most interesting data to show
 df1 <- ddply(df, "NOC", summarise, somme = sum(count(Medal)$freq, na.rm = TRUE))
 ggplot(df1, aes(x = NOC, y = somme, color = NOC)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 df2 <- ddply(df, c("Sport", "Sex"), summarise, somme = sum(count(Sex)$freq))
+# facet_wrap(~Sex)
 ggplot(df2, aes(x = Sport, y = somme, color = Sex)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+# select the most interesting data to show
 df3 <- ddply(df, c("NOC", "Year"), summarise, somme = sum(count(ID)$freq))
 ggplot(df3, aes(x = NOC, y = somme, color = Year)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# attention! sometimes Age = N/A
+df4 <- ddply(df, "Age", summarise, somme = sum(count(Medal)$freq, na.rm = TRUE))
+
+# moyenne = N/A existe
+df5 <- ddply(df, "NOC", summarise, moyenne = mean(Weight, trim = 0, na.rm = TRUE))
+
+
+
