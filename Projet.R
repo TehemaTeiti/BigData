8474cdfd287cb8c537c2b5d9dd388070de42bb14
CHENTing <krystal_0902@icloud.com>
2018-12-14 10:58:21 +0100

    fourth commit

--- Projet.R
+++ Projet.R
@@ -2,10 +2,10 @@
 
 df <- read.csv("athlete_events.csv", sep = ",")
 df1 <- ddply(df, "NOC", summarise, somme = sum(count(Medal)$freq, na.rm = TRUE))
-ggplot(df1, aes(x = NOC, y = somme, color = NOC)) + geom_point()
+ggplot(df1, aes(x = NOC, y = somme, color = NOC)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 df2 <- ddply(df, c("Sport", "Sex"), summarise, somme = sum(count(Sex)$freq))
-ggplot(df2, aes(x = Sport, y = somme, color = Sex)) + geom_point()
+ggplot(df2, aes(x = Sport, y = somme, color = Sex)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 df3 <- ddply(df, c("NOC", "Year"), summarise, somme = sum(count(ID)$freq))
-ggplot(df3, aes(x = Year, y = somme, color = NOC)) + geom_point()
+ggplot(df3, aes(x = NOC, y = somme, color = Year)) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1))
